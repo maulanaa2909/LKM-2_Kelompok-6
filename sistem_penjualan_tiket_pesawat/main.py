b2cff8c3096de5992c544566918d3a299ef5da1b
maulanaa2909 <alyaseirr@gmail.com>
2026-04-20 13:48:23 +0700

    Add .gitignore and update project files

--- sistem_penjualan_tiket_pesawat/main.py
+++ sistem_penjualan_tiket_pesawat/main.py
@@ -1,9 +1,12 @@
 from sistem_pesawat import TiketPesawat
-
+ 
 tiketekonomi = TiketPesawat("penerbangan ekonomi", 500000, 50)
-
-tiketekonomi.tambah_tiket(20)
-
-tiketekonomi.jual_tiket(30)
+ 
+tiketekonomi.tambah_tiket(20)       
+ 
+tiketekonomi.cek_tiket()           
+ 
+tiketekonomi.jual_tiket(30)       
 tiketekonomi.jual_tiket(10)
-
+ 
+tiketekonomi.cek_tiket()
--- sistem_penjualan_tiket_pesawat/sistem_pesawat.py
+++ sistem_penjualan_tiket_pesawat/sistem_pesawat.py
@@ -1,18 +1,21 @@
 class TiketPesawat:
-    def __init__ (self, namapenumpang,hargatiket, jumlah_tiket):
-        self.namapenumpang = namapenumpang
+    def __init__(self, nama_penerbangan, hargatiket, jumlah_tiket):
+        self.nama_penerbangan = nama_penerbangan
         self.hargatiket = hargatiket
         self.jumlah_tiket = jumlah_tiket
-        
+
     def tambah_tiket(self, jumlah):
         self.jumlah_tiket += jumlah
-        print(f'Jumlah tiket yang tersedia: {self.jumlah_tiket}')
-    
-    
+        print(f"Jumlah tiket yang tersedia: {self.jumlah_tiket}")
+
     def jual_tiket(self, jumlah):
-       if self.jumlah_tiket >= jumlah:
-           self.jumlah_tiket -= jumlah
-           total_akhir = self.hargatiket * jumlah
-           print(f"Pembelian berhasil!. harga yang harus dibayar :Rp{total_akhir:,} tiket yang tersisa {self.jumlah_tiket}")
-       else:
-           print("Tiket tidak cukup untuk")
+        if self.jumlah_tiket >= jumlah:
+            self.jumlah_tiket -= jumlah
+            total_akhir = self.hargatiket * jumlah
+            print(f"Pembelian berhasil! Harga yang harus dibayar: Rp{total_akhir:,} | Tiket tersisa: {self.jumlah_tiket}")
+        else:
+            print(f"Tiket tidak cukup! Diminta: {jumlah}, Tersedia: {self.jumlah_tiket}")
+
+    def cek_tiket(self):
+        print(f"Sisa tiket {self.nama_penerbangan}: {self.jumlah_tiket}")
+        return self.jumlah_tiket
